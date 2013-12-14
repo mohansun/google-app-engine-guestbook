a361aed146a07fab90136699c7f70feb3115ab57
Mohan Chinnappan <mohan.chinnappan.np@gmail.com>
2013-12-14 17:30:16 -0500

    more ref links added

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 #http://www.thegeekstuff.com/2010/03/netstat-command-examples/
 
 #https://developers.google.com/appengine/docs/java/tools/maven
+#http://code.google.com/p/appengine-maven-plugin/
 
 # Requires: JDK 1.7 and mvn 3.1.x
 mvn verify
