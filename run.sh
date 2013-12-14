13c9d36b74db8daa16154b47d238ce47ff1c4a45
Mohan Chinnappan <mohan.chinnappan.np@gmail.com>
2013-12-14 17:14:37 -0500

    initial add

---
+++ run.sh
@@ -0,0 +1,8 @@
+#netstat -plnt
+#http://www.thegeekstuff.com/2010/03/netstat-command-examples/
+
+#https://developers.google.com/appengine/docs/java/tools/maven
+
+mvn verify
+mvn appengine:devserver 
+
